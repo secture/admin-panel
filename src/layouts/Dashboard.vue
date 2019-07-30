4915206e420c6758d802e765d84b2704366fd327
Sergio Rodriguez Gundin <sergio@secture.com>
2019-07-30 14:14:25 +0200

    Cerrar sesion text

--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -21,7 +21,7 @@
           </v-flex>
         </v-btn>
         <v-btn flat @click="signOut()">
-          <span>Logout</span>
+          <span>Cerrar sesión</span>
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
@@ -44,41 +44,6 @@
             <v-list-tile-title>{{ item.title }}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-group prepend-icon="account_circle" value="true">
-          <template v-slot:activator>
-            <v-list-tile>
-              <v-list-tile-title>Users</v-list-tile-title>
-            </v-list-tile>
-          </template>
-          <v-list-group no-action sub-group value="true">
-            <template v-slot:activator>
-              <v-list-tile>
-                <v-list-tile-title>Admin</v-list-tile-title>
-              </v-list-tile>
-            </template>
-
-            <v-list-tile v-for="(admin, i) in admins" :key="i">
-              <v-list-tile-title v-text="admin[0]"></v-list-tile-title>
-              <v-list-tile-action>
-                <v-icon v-text="admin[1]"></v-icon>
-              </v-list-tile-action>
-            </v-list-tile>
-          </v-list-group>
-
-          <v-list-group sub-group no-action>
-            <template v-slot:activator>
-              <v-list-tile>
-                <v-list-tile-title>Actions</v-list-tile-title>
-              </v-list-tile>
-            </template>
-            <v-list-tile v-for="(crud, i) in cruds" :key="i">
-              <v-list-tile-title v-text="crud[0]"></v-list-tile-title>
-              <v-list-tile-action>
-                <v-icon v-text="crud[1]"></v-icon>
-              </v-list-tile-action>
-            </v-list-tile>
-          </v-list-group>
-        </v-list-group>
       </v-list>
     </v-navigation-drawer>
     <v-content :class="($vuetify.breakpoint.mdAndDown) ? 'ml-0' : 'ml-300'">
@@ -106,13 +71,10 @@ export default {
   data() {
     return {
       drawer: null,
-      items: [{ title: 'Home', icon: 'dashboard' }],
-      admins: [['Management', 'people_outline'], ['Settings', 'settings']],
-      cruds: [
-        ['Create', 'add'],
-        ['Read', 'insert_drive_file'],
-        ['Update', 'update'],
-        ['Delete', 'delete'],
+      items: [
+        { title: 'Inicio', icon: 'dashboard' },
+        { title: 'Equipos', icon: 'videogame_asset' },
+        { title: 'Jugadores', icon: 'people' },
       ],
     }
   },
