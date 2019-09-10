e38fb4b3320eba0649a7d19c64f58b927cc98dca
Sergio Rodriguez Gundin <sergio@secture.com>
2019-09-10 08:36:53 +0200

    main with aws

--- src/main.ts
+++ src/main.ts
@@ -14,16 +14,11 @@ import { i18n } from '@/plugins/i18n/main'
 import Dashboard from '@/layouts/Dashboard.vue'
 import Default from '@/layouts/Default.vue'
 
-import Amplify from 'aws-amplify'
-import AmplifyAuth from '@aws-amplify/auth'
+import Amplify, * as AmplifyModules from 'aws-amplify'
 import CognitoConfig from '@/api/cognito/config'
 
-console.log(CognitoConfig.Auth.region)
-console.log(CognitoConfig.Auth.userPoolId)
-console.log(CognitoConfig.Auth.userPoolWebClientId)
 Amplify.configure(CognitoConfig)
-Vue.use(AmplifyAuth)
-
+Vue.use(AmplifyModules)
 Vue.config.productionTip = false
 
 Vue.component('dashboard-layout', Dashboard)
