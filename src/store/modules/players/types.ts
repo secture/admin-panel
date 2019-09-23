277bf19972f72d0139da5d0b9ac5c02904952f9f
danielgbullido <daniel.gbullido@gmail.com>
2019-09-23 10:23:35 +0200

    fix typescript conversion

--- src/store/modules/players/types.ts
+++ src/store/modules/players/types.ts
@@ -2,3 +2,4 @@ export const namespace = 'players'
 
 export const GET_PLAYERS = 'GET_PLAYERS'
 export const SET_DATA = 'SET_DATA'
+export const UPDATE_PLAYER = 'UPDATE_PLAYER'
--- src/views/players/Players.vue
+++ src/views/players/Players.vue
@@ -1,91 +1,161 @@
 <template>
-  <v-container fluid>
-    <v-layout align-center justify-center>
-      <v-flex xs12>
-        <v-card>
-          <v-card-title class="d-flex justify-space-around">
-            <span class="headline">{{ title }}</span>
-          </v-card-title>
-          <v-card-text>
-            <v-data-table
-              :headers="headers"
-              :items="players.results"
-              :loading="players.results.length === 0"
-              loading-text="Cargando jugadores"
-              class="elevation-1"
-            >
-              <template v-slot:items="props">
-                <td class="justify-center pa-2">
-                  <img :src="props.item.image96x96" alt />
-                </td>
-                <td class="text-start pa-2">{{ props.item.id }}</td>
-                <td class="text-start pa-2">{{ props.item.name }}</td>
-                <td class="text-start pa-2">{{ props.item.nickname }}</td>
-                <td class="text-start pa-2">{{ props.item.position }}</td>
-                <td class="text-start pa-2">{{ props.item.team.name }}</td>
-                <td class="text-start pa-2">{{ props.item.playerStatus }}</td>
-                <td class="text-start pa-2 layout">
-                  {{ props.item.action }}
-                  <v-btn @click="editTeam(props.item)" icon class="mx-0">
-                    <v-icon color="teal">edit</v-icon>
-                  </v-btn>
-                  <v-btn @click="deleteTeam(props.item)" icon class="mx-0">
-                    <v-icon color="pink">delete</v-icon>
-                  </v-btn>
-                </td>
-              </template>
-            </v-data-table>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+    <v-container fluid>
+        <v-layout align-center justify-center>
+            <v-flex xs12>
+                <v-dialog v-model="dialog" max-width="40em">
+                    <v-card>
+                        <v-card-title>
+                            <span class="headline">{{ formTitle }}</span>
+                        </v-card-title>
+                        <v-card-text class="px-0">
+                            <v-container>
+                                <v-layout row>
+                                    <v-flex md12>
+                                        <v-text-field v-model="editedPlayer.name" label="Name"></v-text-field>
+                                        <v-text-field v-model="editedPlayer.nickname" label="Nickname"></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                            </v-container>
+                        </v-card-text>
+                        <v-card-actions class="justify-self-end pa-4">
+                            <v-spacer></v-spacer>
+                            <v-btn flat color="pink" text @click="close">Cancel</v-btn>
+                            <v-btn dark color="teal" text @click="confirmEditPlayer()">Save</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+                <v-card>
+                    <v-card-title class="d-flex justify-space-around">
+                        <span class="headline">{{ title }}</span>
+                        <v-text-field
+                                v-model="search"
+                                append-icon="search"
+                                label="Search"
+                                single-line
+                                hide-details
+                                class="pa-0 ma-0"
+                        ></v-text-field>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-data-table
+                                :headers="headers"
+                                :items="players.results"
+                                :loading="players.results.length === 0"
+                                loading-text="Cargando jugadores"
+                                class="elevation-1"
+                        >
+                            <template v-slot:items="props">
+                                <td class="justify-center pa-2">
+                                    <img :src="props.item.image96x96" alt/>
+                                </td>
+                                <td class="text-start pa-2">{{ props.item.id }}</td>
+                                <td class="text-start pa-2">{{ props.item.name }}</td>
+                                <td class="text-start pa-2">{{ props.item.nickname }}</td>
+                                <td class="text-start pa-2">{{ props.item.position }}</td>
+                                <td class="text-start pa-2">{{ props.item.team.name }}</td>
+                                <td class="text-start pa-2">{{ props.item.playerStatus }}</td>
+                                <td class="text-start pa-2 layout">
+                                    {{ props.item.action }}
+                                    <v-btn icon class="mx-0" @click="editPlayer(props.item)">
+                                        <v-icon color="teal">edit</v-icon>
+                                    </v-btn>
+                                    <v-btn icon class="mx-0" @click="deletePlayer(props.item)">
+                                        <v-icon color="pink">delete</v-icon>
+                                    </v-btn>
+                                </td>
+                            </template>
+                        </v-data-table>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator'
-import * as getters from '@/store/modules/players/getters'
-import * as types from '@/store/modules/players/types'
+  import { Component, Vue, Watch } from 'vue-property-decorator'
+  import * as getters from '@/store/modules/players/getters'
+  import * as types from '@/store/modules/players/types'
 
-import { Action, Getter } from 'vuex-class'
-import { InfoPlayers, DataPlayers } from '@/models/players'
-const namespace: string = types.namespace
+  import { Action, Getter } from 'vuex-class'
+  import { InfoPlayers, DataPlayers } from '@/models/players'
 
-@Component
-export default class Players extends Vue {
-  @Action(types.GET_PLAYERS, { namespace }) getPlayers: any
-  @Getter(getters.GET_DATA, { namespace }) playersStored!: DataPlayers
+  const namespace: string = types.namespace
 
-  public title: string = 'Listado de jugadores'
-  public headers: Array<any> = [
-    { text: 'image', align: 'left', value: 'image96x96' },
-    { text: 'id', align: 'left', value: 'id' },
-    { text: 'Name', align: 'left', value: 'name' },
-    { text: 'Nickname', align: 'left', value: 'nickname' },
-    { text: 'Posición', align: 'left', value: 'position' },
-    { text: 'Equípo', align: 'left', value: 'team.name' },
-    { text: 'Estado', align: 'left', value: 'playerStatus' },
-    { text: 'Actions', value: 'action', sortable: false },
-  ]
+  @Component
+  export default class Players extends Vue {
+    @Action(types.GET_PLAYERS, { namespace }) getPlayers: any
+    @Action(types.UPDATE_PLAYER, { namespace }) updatedPlayer: any
+    @Getter(getters.GET_DATA, { namespace }) playersStored!: DataPlayers
 
-  public players: DataPlayers = {
-    page: 0,
-    pageSize: 0,
-    results: [],
-    totalResults: 0,
-  }
+    public title: string = 'Listado de jugadores'
+    public formTitle: string = 'Buscar equipo'
+    public search: string = ''
+    public dialog: boolean = false
+    public headers: Array<any> = [
+      { text: 'image', align: 'left', value: 'image96x96' },
+      { text: 'id', align: 'left', value: 'id' },
+      { text: 'Name', align: 'left', value: 'name' },
+      { text: 'Nickname', align: 'left', value: 'nickname' },
+      { text: 'Posición', align: 'left', value: 'position' },
+      { text: 'Equípo', align: 'left', value: 'team.name' },
+      { text: 'Estado', align: 'left', value: 'playerStatus' },
+      { text: 'Actions', value: 'action', sortable: false },
+    ]
 
-  mounted() {
-    if (this.playersStored.results === null) {
-      this.getPlayers().then(playersMasters => {
-        if (playersMasters !== null) {
-          this.players = playersMasters
+    public players: DataPlayers = {
+      page: 0,
+      pageSize: 0,
+      results: [],
+      totalResults: 0,
+    }
+    public editedPlayer: any = {
+      name: '',
+      nickname: '',
+    }
+
+    @Watch('dialog')
+    onDialogChange(value) {
+      value || this.close()
+    }
+
+    mounted() {
+      if (this.playersStored.results === null) {
+        this.getPlayers().then(playersMasters => {
+          if (playersMasters !== null) {
+            this.players = playersMasters
+          }
+        })
+      } else {
+        this.players = this.playersStored
+      }
+    }
+
+    confirmEditPlayer() {
+      this.updatedPlayer(this.editedPlayer).then(response => {
+        if (response !== null) {
+          this.getPlayers().then(players => {
+            if (players !== null) {
+              this.players = players
+            }
+          })
         }
       })
-    } else {
-      this.players = this.playersStored
+      this.close()
+    }
+
+    editPlayer(player) {
+      this.editedPlayer = Object.assign({}, player)
+      this.dialog = true
+    }
+
+    deletePlayer(player) {
+      console.log(player)
+    }
+
+    close() {
+      this.dialog = false
     }
   }
-}
 </script>
 <style>
 </style>
